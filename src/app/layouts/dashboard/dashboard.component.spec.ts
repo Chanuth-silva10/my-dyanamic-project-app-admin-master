92bf7f4ccfc0a5159f65a82cf2fb783be321f816
Chanuth <it20074418@my.sliit.lk>
2023-11-26 23:42:40 +0530

    convert angular 16 version

--- src/app/layouts/dashboard/dashboard.component.spec.ts
+++ src/app/layouts/dashboard/dashboard.component.spec.ts
@@ -6,12 +6,10 @@ describe('DashboardComponent', () => {
   let component: DashboardComponent;
   let fixture: ComponentFixture<DashboardComponent>;
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
+  beforeEach(() => {
+    TestBed.configureTestingModule({
       declarations: [DashboardComponent]
-    })
-    .compileComponents();
-    
+    });
     fixture = TestBed.createComponent(DashboardComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
