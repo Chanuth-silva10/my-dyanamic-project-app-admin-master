c07ee43ab19c01146187bda75971a0d125addba9
Chanuth <it20074418@my.sliit.lk>
2023-11-28 02:31:40 +0530

    load all post data

--- src/app/services/posts.service.ts
+++ src/app/services/posts.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { AngularFireStorage } from '@angular/fire/compat/storage';
 import { AngularFirestore } from '@angular/fire/compat/firestore';
 import { ToastrService } from 'ngx-toastr';
+import { map } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root',
@@ -28,6 +29,21 @@ export class PostsService {
           this.saveData(postData);
         });
     });
+  }
+
+  loadData() {
+    return this.afs
+      .collection('posts')
+      .snapshotChanges()
+      .pipe(
+        map((actions) => {
+          return actions.map((a) => {
+            const data = a.payload.doc.data();
+            const id = a.payload.doc.id;
+            return { id, data };
+          });
+        })
+      );
   }
 
   saveData(postData: any) {
