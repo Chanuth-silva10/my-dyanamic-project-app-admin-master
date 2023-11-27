1ee03269fec287c74862d02a05627622ff579f73
Chanuth <it20074418@my.sliit.lk>
2023-11-27 11:19:27 +0530

    separate category from interface

--- src/app/categories/categories.component.ts
+++ src/app/categories/categories.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { AngularFirestore } from '@angular/fire/compat/firestore';
+import { CategoriesService } from '../services/categories.service';
+import { Category } from '../models/category';
 
 @Component({
   selector: 'app-categories',
@@ -7,48 +8,17 @@ import { AngularFirestore } from '@angular/fire/compat/firestore';
   styleUrls: ['./categories.component.css'],
 })
 export class CategoriesComponent implements OnInit {
-  constructor(private afs: AngularFirestore) {}
+  constructor( private categoryService: CategoriesService) {}
 
   ngOnInit(): void {}
 
   onSubmit(formData: { value: { category: string } }) {
-    let categoryData = {
-      category: formData.value.category,
-      status: 'active',
-    };
 
-    let subCategoryData = {
-      subCategory: 'subCategory1',
-    };
+    let categoryData: Category = {
+      category: formData.value.category
+    };   
+    
+    this.categoryService.saveData(categoryData);
 
-    this.afs
-      .collection('categories')
-      .add(categoryData)
-      .then((docRef) => {
-        console.log(docRef);
-
-        this.afs
-          .collection('categories')
-          .doc(docRef.id)
-          .collection('subcategories')
-          .add(subCategoryData)
-          .then((docRef1) => {
-            console.log(docRef1);
-
-            this.afs
-              .collection('categories')
-              .doc(docRef.id)
-              .collection('subcategories')
-              .doc(docRef1.id)
-              .collection('subsubcategories')
-              .add(subCategoryData)
-              .then((docRef2) => {
-                console.log('Second Level Subcategory Saved Successfully');
-              });
-          });
-      })
-      .catch((err) => {
-        console.log(err);
-      });
   }
 }
