588ef3c1b8e653f6292387820c83e268c6d29151
Chanuth <it20074418@my.sliit.lk>
2023-11-28 15:28:29 +0530

    logout create

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AngularFireModule } from '@angular/fire/compat';
 import { AngularFirestoreModule } from '@angular/fire/compat/firestore';
 import { AngularFireStorageModule } from '@angular/fire/compat/storage';
+import { AngularFireAuthModule } from '@angular/fire/compat/auth';
 
 import { FormsModule, ReactiveFormsModule} from '@angular/forms';
 import { ToastrModule } from 'ngx-toastr';
@@ -46,6 +47,7 @@ import { LoginComponent } from './auth/login/login.component';
     AngularEditorModule,
     HttpClientModule,
     AngularFireStorageModule,
+    AngularFireAuthModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -1,18 +1,16 @@
-import { Component , OnInit } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AuthService } from 'src/app/services/auth.service';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+  styleUrls: ['./login.component.css'],
 })
-export class LoginComponent implements OnInit{
-   constructor(){
-   }
-  ngOnInit(): void {
-    
-  }
+export class LoginComponent implements OnInit {
+  constructor(private authService: AuthService) {}
+  ngOnInit(): void {}
 
-  onSubmit(formValue: any){
-    console.log(formValue);
+  onSubmit(formValue: any) {
+    this.authService.login(formValue.email, formValue.password);
   }
 }
