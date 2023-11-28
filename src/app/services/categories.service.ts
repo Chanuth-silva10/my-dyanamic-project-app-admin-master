4eada16c9b320baddcd2e107a24d86bc3e63ec93
Chanuth <it20074418@my.sliit.lk>
2023-11-28 15:50:44 +0530

    allocate new page

--- src/app/services/categories.service.ts
+++ src/app/services/categories.service.ts
@@ -10,47 +10,13 @@ export class CategoriesService {
   constructor(private afs: AngularFirestore, private toastr: ToastrService) {}
 
   saveData(data: any) {
-    this.afs
-      .collection('categories')
-      .add(data)
-      .then((docRef) => {
-        this.toastr.success('Data Insert Successfully ..!');
-      })
-      .catch((err) => {
-        console.log(err);
-      });
-  }
-
-  loadData() {
-    return this.afs
-      .collection('categories')
-      .snapshotChanges()
-      .pipe(
-        map((actions) => {
-          return actions.map((a) => {
-            const data = a.payload.doc.data();
-            const id = a.payload.doc.id;
-            return { id, data };
-          });
-        })
-      );
   }
 
   updataData(id:string, EditData:object) {
-    this.afs
-      .doc(`categories/${id}`)
-      .update(EditData)
-      .then((docRef) => {
-        this.toastr.success('Data Updated Successfully ..!');
-      });
+   
   }
 
   deleteData(id:string) {
-    this.afs
-      .doc(`categories/${id}`)
-      .delete()
-      .then((docRef) => {
-        this.toastr.success('Data Deleted Successfully ..!');
-      });
+  
   }
 }
