82639224d047b6de1ccc75e4002c90d44017e7a9
Chanuth <it20074418@my.sliit.lk>
2023-11-28 11:47:38 +0530

    create login page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,10 +4,12 @@ import { DashboardComponent } from './layouts/dashboard/dashboard.component';
 import { CategoriesComponent } from './categories/categories.component';
 import { AllPostComponent } from './posts/all-post/all-post.component';
 import { NewPostComponent } from './posts/new-post/new-post.component';
+import { LoginComponent } from './auth/login/login.component';
 
 const routes: Routes = [
   { path: '', component: DashboardComponent },
   { path: 'categories', component: CategoriesComponent },
+  { path: 'login', component: LoginComponent },
 
   { path: 'posts', component: AllPostComponent },
   { path: 'posts/new', component:  NewPostComponent},
