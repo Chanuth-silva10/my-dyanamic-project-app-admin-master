b858fbe68401cab42327daeb389b6294bc8bcf1e
Chanuth <it20074418@my.sliit.lk>
2023-11-28 15:51:41 +0530

    fix bug

--- src/app/services/posts.service.ts
+++ src/app/services/posts.service.ts
@@ -16,40 +16,8 @@ export class PostsService {
     private router: Router
   ) {}
 
-  uploadImage(selectedImage: any, postData: any, formStatus: any, id: any) {
-    const filePath = `postIMG/${Date.now()}`;
-
-    this.storage.upload(filePath, selectedImage).then(() => {
-      console.log('Post image uploaded successfully.');
-
-      this.storage
-        .ref(filePath)
-        .getDownloadURL()
-        .subscribe((URL) => {
-          postData.postImgPath = URL;
-
-          if (formStatus == 'Edit') {
-            this.updateData(id, postData);
-          } else {
-            this.saveData(postData);
-          }
-        });
-    });
-  }
-
   loadData() {
     return this.afs
-      .collection('posts')
-      .snapshotChanges()
-      .pipe(
-        map((actions) => {
-          return actions.map((a) => {
-            const data = a.payload.doc.data();
-            const id = a.payload.doc.id;
-            return { id, data };
-          });
-        })
-      );
   }
 
   loadOneData(id: any) {
@@ -57,43 +25,14 @@ export class PostsService {
   }
 
   saveData(postData: any) {
-    this.afs
-      .collection('posts')
-      .add(postData)
-      .then((docRef) => {
-        this.toastr.success('Post Data Insert Successfully ..!');
-        this.router.navigate(['/posts']);
-      });
   }
 
   updateData(id: any, postData: any) {
-    this.afs
-      .doc(`posts/${id}`)
-      .update(postData)
-      .then(() => {
-        this.toastr.success('Post Data Update Successfully ..!');
-        this.router.navigate(['/posts']);
-      });
   }
 
   deleteImage(postImgPath: any, id: any) {
-    this.storage.storage.refFromURL(postImgPath).delete().then(()=>{
-      this.deleteData(id);
-    })
   }
 
   deleteData(id:any){
-    this.afs.doc(`posts/${id}`).delete().then(()=>{
-      this.toastr.warning('Delete Successfully ..!');
-    })
-  }
-
-  markFeatured(id:any, featuredData: any){
-    this.afs
-      .doc(`posts/${id}`)
-      .update(featuredData)
-      .then(() => {
-        this.toastr.info('Featured Status Updated!');
-      });
   }
 }
